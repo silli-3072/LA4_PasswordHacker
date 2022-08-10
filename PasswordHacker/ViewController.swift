4514977de1837f76e64d96dd006de80119b1f16c
MinoriHaruta <hrtmnr.3072@gmail.com>
2022-08-10 14:44:11 +0900

    🐮

--- PasswordHacker/ViewController.swift
+++ PasswordHacker/ViewController.swift
@@ -8,12 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var resultLabel1: UILabel!
+    @IBOutlet var resultLabel2: UILabel!
+    @IBOutlet var resultLabel3: UILabel!
+    @IBOutlet var resultLabel4: UILabel!
+    
+    @IBOutlet var countLabel: UILabel!
+    
+    var password: Int = 1234
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func startButton(){
+        for i in 0...9999 {
+            countLabel.text = String(i)
+            RunLoop.main.run(until: Date(timeIntervalSinceNow: 0.0005))
+            
+            
+            if i == password {
+                var digits = [Int]()
+                
+                for _ in 0...3 {
+                    digits.append(password % 10)
+                    password = password / 10
+                    
+                }
+                
+                resultLabel4.text = String(digits[0])
+                resultLabel3.text = String(digits[1])
+                resultLabel2.text = String(digits[2])
+                resultLabel1.text = String(digits[3])
+            }
+            
+            
+        }
+        
+    }
+    
+    @IBAction func resetButton(){
+        password = 1234
+        countLabel.text = "スタートボタンを押して解析開始"
+        
+        resultLabel4.text = "0"
+        resultLabel3.text = "0"
+        resultLabel2.text = "0"
+        resultLabel1.text = "0"
+        
+        
+    }
+    
+    
 }
 
